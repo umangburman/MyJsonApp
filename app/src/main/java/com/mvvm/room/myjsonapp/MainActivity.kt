0f2fd178d1629a139064eea8c016df7cc60aa292
umanglogixtic <umang_burman@logixtic.co.in>
2020-06-24 21:42:24 +0530

    Initial commit

---
+++ app/src/main/java/com/mvvm/room/myjsonapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mvvm.room.myjsonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
