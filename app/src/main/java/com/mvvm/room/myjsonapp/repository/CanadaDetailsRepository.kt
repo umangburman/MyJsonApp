0ed95d22ef75474498e89c2c53763172460e0096
Umang Burman <umang.burman.micro@gmail.com>
2020-06-24 23:17:50 +0530

    Third Commit
    Signed-off-by: Umang Burman <umang.burman.micro@gmail.com>

--- app/src/main/java/com/mvvm/room/myjsonapp/repository/CanadaDetailsRepository.kt
+++ app/src/main/java/com/mvvm/room/myjsonapp/repository/CanadaDetailsRepository.kt
@@ -1,4 +1,29 @@
 package com.mvvm.room.myjsonapp.repository
 
-class CanadaDetailsRepository {
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import com.mvvm.room.myjsonapp.model.CanadaTableModel
+import com.mvvm.room.myjsonapp.retrofit.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CanadaDetailsRepository : Callback<CanadaTableModel> {
+
+    lateinit var canadaDetailsLiveData: MutableLiveData<CanadaTableModel>
+
+    fun getDataFromApi(context: Context) {
+
+        val call = RetrofitClient.apiInterface
+    }
+
+    override fun onFailure(call: Call<CanadaTableModel>, t: Throwable) {
+        // - - TODO("Not yet implemented")
+        canadaDetailsLiveData.value = null
+    }
+
+    override fun onResponse(call: Call<CanadaTableModel>, response: Response<CanadaTableModel>) {
+        // - - TODO("Not yet implemented")
+        canadaDetailsLiveData.value = response.body()
+    }
 }
--- app/src/main/java/com/mvvm/room/myjsonapp/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/mvvm/room/myjsonapp/retrofit/RetrofitClient.kt
@@ -1,4 +1,38 @@
 package com.mvvm.room.myjsonapp.retrofit
 
-class RetrofitClient {
+import com.mvvm.room.myjsonapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    val MainServer = "https://api.github.com/"
+
+    val retrofitClient: Retrofit.Builder by lazy {
+
+        val levelType: HttpLoggingInterceptor.Level
+        if (BuildConfig.BUILD_TYPE.contentEquals("debug"))
+            levelType = HttpLoggingInterceptor.Level.BODY else levelType =
+            HttpLoggingInterceptor.Level.NONE
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(levelType)
+
+        val okhttpClient = OkHttpClient.Builder()
+        okhttpClient.addInterceptor(logging)
+
+        Retrofit.Builder()
+            .baseUrl(MainServer)
+            .client(okhttpClient.build())
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    val apiInterface: ApiInterface by lazy {
+        retrofitClient
+            .build()
+            .create(ApiInterface::class.java)
+    }
+
 }
