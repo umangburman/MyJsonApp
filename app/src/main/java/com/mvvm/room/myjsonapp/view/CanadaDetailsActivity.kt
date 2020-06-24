852ccff5fc0cbe9990232ab9a92231fe346860bc
Umang Burman <umang.burman.micro@gmail.com>
2020-06-24 22:36:38 +0530

    Third Commit
    Signed-off-by: Umang Burman <umang.burman.micro@gmail.com>

--- app/src/main/java/com/mvvm/room/myjsonapp/view/CanadaDetailsActivity.kt
+++ app/src/main/java/com/mvvm/room/myjsonapp/view/CanadaDetailsActivity.kt
@@ -1,14 +1,27 @@
 package com.mvvm.room.myjsonapp.view
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.mvvm.room.myjsonapp.R
+import com.mvvm.room.myjsonapp.viewmodel.CanadaDetailsViewModel
 
 class CanadaDetailsActivity : AppCompatActivity() {
 
+    lateinit var context: Context
+
+    lateinit var canadaDetailsViewModel: CanadaDetailsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canadadetails)
+
+        context = this@CanadaDetailsActivity
+
+        canadaDetailsViewModel = ViewModelProvider(this)
+            .get(CanadaDetailsViewModel::class.java)
+
+
     }
 }
