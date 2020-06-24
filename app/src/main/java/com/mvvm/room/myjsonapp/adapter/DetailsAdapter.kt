ccdcdd111eab22d636f91b6abe6af63ca47667da
Umang Burman <umang.burman.micro@gmail.com>
2020-06-25 01:26:09 +0530

    Sixth changelist
    Signed-off-by: Umang Burman <umang.burman.micro@gmail.com>

--- app/src/main/java/com/mvvm/room/myjsonapp/adapter/DetailsAdapter.kt
+++ app/src/main/java/com/mvvm/room/myjsonapp/adapter/DetailsAdapter.kt
@@ -1,6 +1,48 @@
 package com.mvvm.room.myjsonapp.adapter
 
-class DetailsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mvvm.room.myjsonapp.R
+import com.mvvm.room.myjsonapp.model.RowsData
+import kotlinx.android.synthetic.main.list_item_details.view.*
 
+class DetailsAdapter constructor(val context: Context, val userList: List<RowsData>) :
+    RecyclerView.Adapter<DetailsAdapter.DetailsHolder>() {
 
+    class DetailsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var lblTitle = itemView.lblTitleAnswer
+        var lblDescription = itemView.lblDescriptionAnswer
+        var imgReference = itemView.imgHref
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailsHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_details, parent, false)
+
+        return DetailsHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DetailsHolder, position: Int) {
+
+        val detailsData: RowsData = userList.get(position)
+
+        holder.lblTitle.text = detailsData.title?.let { it -> "- - -" }
+        holder.lblDescription.text = detailsData.description?.let { it -> "- - -" }
+
+        val url = detailsData.imageHref
+        Glide.with(context)
+            .load(url)
+            .centerCrop()
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.ic_no_image_found)
+            .into(holder.imgReference)
+    }
+
+    override fun getItemCount(): Int = userList.size
 }
--- app/src/main/java/com/mvvm/room/myjsonapp/view/DetailsActivity.kt
+++ app/src/main/java/com/mvvm/room/myjsonapp/view/DetailsActivity.kt
@@ -2,11 +2,14 @@ package com.mvvm.room.myjsonapp.view
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mvvm.room.myjsonapp.R
-import com.mvvm.room.myjsonapp.model.RowsData
+import com.mvvm.room.myjsonapp.adapter.DetailsAdapter
 import com.mvvm.room.myjsonapp.viewmodel.DetailsViewModel
 import kotlinx.android.synthetic.main.activity_details.*
 
@@ -15,8 +18,6 @@ class DetailsActivity : AppCompatActivity() {
     lateinit var context: Context
 
     lateinit var detailsViewModel: DetailsViewModel
-
-    lateinit var detailsList: List<RowsData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +30,30 @@ class DetailsActivity : AppCompatActivity() {
             title = "- - -"
         }
 
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        }
+
         detailsViewModel = ViewModelProvider(this)
             .get(DetailsViewModel::class.java)
 
-        detailsViewModel.getDataFromApi(context).observe(this, Observer {
+        detailsViewModel.getDataFromApi(context).observe(this, Observer { tableModel ->
 
-            supportActionBar.apply {
-                title = it.title
+            if (tableModel == null) {
+                recyclerView.visibility = View.GONE
+                noDataConstraint.visibility = View.VISIBLE
+            } else {
+
+                supportActionBar.apply {
+                    title = tableModel.title
+                }
+
+                val detailsAdapter = tableModel.rows?.let { it -> DetailsAdapter(context, it) }
+                recyclerView.apply {
+                    adapter = detailsAdapter
+                }
             }
-
 
         })
     }
